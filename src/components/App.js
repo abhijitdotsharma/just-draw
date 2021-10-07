444cfe0a86f234e2f8e8e5146614f2362205c586
wickedSharma <61442144+abhijitdotsharma@users.noreply.github.com>
2021-10-07 22:30:23 +0530

    Update - App.js with simple mouse movment
    Draws a line on mouse click, till onMouseUp.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const App = () => {
 
+    const canvasRef = useRef(null)
+    const contextRef = useRef(null)
+
+    let isDrawing = false;
+
+    //startDrawing
+
+    //endDrawing
+
+    //draw
+
+    useEffect( () => {
+
+        //define myCanvas
+        const myCanvas = canvasRef.current; // same as getElementByID
+        myCanvas.width = window.innerWidth - 100
+        myCanvas.height = window.innerHeight;
+
+        const context = myCanvas.getContext("2d")
+        
+        context.scale(1,1)
+        context.lineWidth = 7;
+        context.lineCap = 'round'
+        context.strokeStyle = 'black'
+        //preserve the context b/w render, to use in drawing functions
+        contextRef.current = context
+        
+    }, [])
+
+    //begin path
+    //moveto
+    //lineto
+    //stroke
+
+    //to get coordinates of mouse -> event.clientX, .clientY
+    const startDrawing = (event) => {
+        isDrawing = true;
+        console.log('mouse down')
+    
+        contextRef.current.beginPath();
+        
+    }
+
+    const endDrawing = () => {
+        console.log('mouse up')
+        isDrawing = false
+    }
+
+    const draw = (event) => {
+        if(!isDrawing){
+            return
+        }
+
+        contextRef.current.moveTo(event.clientX, event.clientY)
+        contextRef.current.lineTo(event.clientX, event.clientY)
+        contextRef.current.stroke()
+        //context.moveTo(x, y)
+
+    }
+
     return(
-        <canvas />
+        <canvas 
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseMove={draw}
+        />
     )
 }
 
